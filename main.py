11da8c5f11686323e17931550e2b3c261340ae68
Artur Mustafin <funcelot@outlook.com>
2022-01-11 10:38:01 +0300

    refactor and added multiline

--- main.py
+++ main.py
@@ -48,6 +48,10 @@ class Text:
     debug: str = ""
 
 
+class DateTime:
+    date:str = ""
+
+
 class MainScreenMouseProcessor(IGameEventProcessor):
     def process(self):
         mouse_pos = pygame.mouse.get_pos()
@@ -61,52 +65,62 @@ class MainScreenKeyboardProcessor(IGameEventProcessor):
 
         keys = pygame.key.get_pressed()
         if keys[pygame.K_UP]:
-            Keyboard.key = f"UP"
+            Keyboard.key = "UP"
 
         if keys[pygame.K_LEFT]:
-            Keyboard.key = f"LEFT"
+            Keyboard.key = "LEFT"
 
         if keys[pygame.K_RIGHT]:
-            Keyboard.key = f"RIGHT"
+            Keyboard.key = "RIGHT"
 
         if keys[pygame.K_DOWN]:
-            Keyboard.key = f"DOWN"
+            Keyboard.key = "DOWN"
 
         if keys[pygame.K_q]:
             Keyboard.key = "Q"
 
 
+class MainScreenDateTimeProcessor(IGameEventProcessor):
+    def process(self):
+        DateTime.date = f"{datetime.fromtimestamp(Game.ticks_ms() / 1000).strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]}"
+
+
 class MainScreenDebugProcessor(IGameEventProcessor):
     def process(self):
-        Text.debug = f"{(Mouse.position[0], Mouse.position[1])} {'DOWN' if Mouse.button_down else ''} {Keyboard.key}"
+        Text.debug = f"{DateTime.date}|{(Mouse.position[0], Mouse.position[1])}|{'MOUSEDOWN' if Mouse.button_down else ''}|{Keyboard.key}"
+
+
+class ISurface(pygame.Surface):
+    def __init__(self, surface: pygame.Surface):
+        self.surface = surface
+
 
 class Painter:
-    def __init__(self, size:tuple[int, int]):
-        self.size = size
-        self.ticks = Game.ticks_ms()
-        self.date = datetime.fromtimestamp(self.ticks / 1000).strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
-        self.text_surface = Game.font.render(self.date, False, (0, 0, 0))
-        self.screen = pygame.display.set_mode(self.size, flags=pygame.NOFRAME, vsync=1)
-
-    def paint(self) -> None:
-        self.date = datetime.fromtimestamp(Game.ticks_ms() / 1000).strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
-        text_surface = Game.font.render(Text.debug, True, (255, 255, 255))
-        text_rect = text_surface.get_rect()
-        self.screen.fill((0, 0, 0))
-        self.screen.blit(text_surface, text_rect)
+    def paint(self, surface: ISurface) -> None:
+        surface.surface.fill((0, 0, 0))
+        top: int = 0
+        for line in Text.debug.split("|"):
+            text_surface = Game.font.render(line, True, (255, 255, 255))
+            text_rect = text_surface.get_rect()
+            text_rect.top = top
+            text_rect.left = 0
+            top += text_rect.height
+            surface.surface.blit(text_surface, text_rect)
 
 
-class Game:
+class Game(ISurface):
     fps = 60
     caption = "Dungeon Warrior"
     icon = pygame.image.load(os.path.join("images", "dungeon.png"))
     font = pygame.font.Font(os.path.join("fonts", "pt-mono.ttf"), 64)
 
     def __init__(self, size: tuple[int, int] = (1920, 1080)):
-        self.mouse_processor = MainScreenMouseProcessor()
+        super().__init__(pygame.display.set_mode(size, flags=pygame.NOFRAME, vsync=1))
         self.keyboard_processor = MainScreenKeyboardProcessor()
+        self.mouse_processor = MainScreenMouseProcessor()
+        self.datetime_processor = MainScreenDateTimeProcessor()
         self.debug_processor = MainScreenDebugProcessor()
-        self.painter = Painter(size)
+        self.painter = Painter()
 
     def init(self) -> None:
         pygame.display.set_caption(Game.caption)
@@ -130,9 +144,9 @@ class Game:
 
             self.keyboard_processor.process()
             self.mouse_processor.process()
+            self.datetime_processor.process()
             self.debug_processor.process()
-            self.painter.paint()
-
+            self.painter.paint(self)
             self.clock.tick(Game.fps)
 
             pygame.display.flip()
@@ -140,6 +154,7 @@ class Game:
     @staticmethod
     def ticks_ms() -> int:
         return int(round(time.time() * 1000))
+
 
 @decorator_factory(debug=True)
 def main():

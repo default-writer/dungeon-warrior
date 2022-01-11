c58eeb6efe7a6eaa39f770ffe4ee0a2eafe7e071
Artur Mustafin <funcelot@outlook.com>
2022-01-11 08:48:11 +0300

    code refactoring

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ from datetime import datetime
 import pygame
 
 
+pygame.init()
+pygame.font.init()
+
+
 def decorator_factory(debug=False):
     def decorator(function):
         def wrapper(*args, **kwargs):
@@ -24,12 +28,6 @@ class GameStates:
 
 date_format = "%Y-%m-%d %H:%M:%S.%f"
 
-class IGameState:
-    def __init__(self):
-        self.key = None
-        self.debug: str = ""
-        self.counter: int = 0
-
 
 class IGameEventProcessor:
     def process(self):
@@ -44,6 +42,10 @@ class Mouse:
 
 class Keyboard:
     key: str = ""
+
+
+class Text:
+    debug: str = ""
 
 
 class MainScreenMouseProcessor(IGameEventProcessor):
@@ -74,87 +76,71 @@ class MainScreenKeyboardProcessor(IGameEventProcessor):
             Keyboard.key = "Q"
 
 
-class Game(IGameState):
-    def __init__(self, size: tuple[int, int] = (1920, 1080)):
-        super().__init__()
-        pygame.init()
-        pygame.font.init()
-        self.caption = "Dungeon Warrior"
+class MainScreenDebugProcessor(IGameEventProcessor):
+    def process(self):
+        Text.debug = f"{(Mouse.position[0], Mouse.position[1])} {'DOWN' if Mouse.button_down else ''} {Keyboard.key}"
+
+class Painter:
+    def __init__(self, size:tuple[int, int]):
         self.size = size
-        self.FPS = 60
-        self.state = GameStates.INIT
         self.ticks = Game.ticks_ms()
-        self.font = None
-        self.icon = None
-        self.screen = None
-        self.clock = None
-        self.text_surface = None
-        self.date = datetime.fromtimestamp(self.ticks / 1000).strftime(date_format)[:-3]
-        self.icon = pygame.image.load(os.path.join("images", "dungeon.png"))
-        self.font = pygame.font.Font(os.path.join("fonts", "pt-mono.ttf"), 16)
+        self.date = datetime.fromtimestamp(self.ticks / 1000).strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
+        self.text_surface = Game.font.render(self.date, False, (0, 0, 0))
+        self.screen = pygame.display.set_mode(self.size, flags=pygame.NOFRAME, vsync=1)
+
+    def paint(self) -> None:
+        self.date = datetime.fromtimestamp(Game.ticks_ms() / 1000).strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
+        text_surface = Game.font.render(Text.debug, True, (255, 255, 255))
+        text_rect = text_surface.get_rect()
+        self.screen.fill((0, 0, 0))
+        self.screen.blit(text_surface, text_rect)
+
+
+class Game:
+    fps = 60
+    caption = "Dungeon Warrior"
+    icon = pygame.image.load(os.path.join("images", "dungeon.png"))
+    font = pygame.font.Font(os.path.join("fonts", "pt-mono.ttf"), 64)
+
+    def __init__(self, size: tuple[int, int] = (1920, 1080)):
+        pygame.display.set_caption(Game.caption)
+        pygame.display.set_icon(Game.icon)
+        self.clock = pygame.time.Clock()
         self.mouse_processor = MainScreenMouseProcessor()
         self.keyboard_processor = MainScreenKeyboardProcessor()
-        self.run = False
+        self.debug_processor = MainScreenDebugProcessor()
+        self.painter = Painter(size)
+
 
     def init(self) -> None:
-        pygame.display.set_caption(self.caption)
-        pygame.display.set_icon(self.icon)
-        self.screen = pygame.display.set_mode(self.size, flags=pygame.NOFRAME, vsync=1)
-        self.clock = pygame.time.Clock()
-        self.date = datetime.fromtimestamp(self.ticks / 1000).strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
-        self.text_surface = self.font.render(self.date, False, (0, 0, 0))
-        self.state = GameStates.DRAW
-        self.run = True
         print("Game started!")
 
     def quit(self) -> None:
         print("Game exited!")
-        self.state = GameStates.QUIT
         pygame.quit()
 
     def draw(self) -> None:
-        while self.run:
+        while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT or Keyboard.key == "Q":
-                    self.run = False
+                    return
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     Mouse.button_down = True
                 if event.type == pygame.MOUSEBUTTONUP:
                     Mouse.button_down = False
 
-
             self.keyboard_processor.process()
             self.mouse_processor.process()
-            self.debug = f"{(Mouse.position[0], Mouse.position[1])} {'DOWN' if Mouse.button_down else ''} {Keyboard.key}"
+            self.debug_processor.process()
+            self.painter.paint()
 
-            self.paint()
-            self.update()
-            
-            self.clock.tick(self.FPS)
+            self.clock.tick(Game.fps)
+
             pygame.display.flip()
-
-    def text_objects(self, text, font):
-        self.text_surface = font.render(text, True, (255, 255, 255))
-        return self.text_surface, self.text_surface.get_rect()
-
-    def update(self):
-        self.ticks = Game.ticks_ms()
-        self.date = datetime.fromtimestamp(self.ticks / 1000).strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
-
-    def paint(self) -> None:
-        text_surface, text_rect = self.text_objects(self.debug, self.font)
-        # x = 0
-        # y = 0
-        # w = text_rect.width
-        # h = text_rect.height
-        # text_rect.center = ((x + (w/2)), (y + (h/2)))
-        self.screen.fill((0, 0, 0))
-        self.screen.blit(text_surface, text_rect)
 
     @staticmethod
     def ticks_ms() -> int:
         return int(round(time.time() * 1000))
-
 
 @decorator_factory(debug=True)
 def main():

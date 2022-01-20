1a22b1920f93843e62799fc0ffcade0846b8d1b6
Artur Mustafin <funcelot@outlook.com>
2022-01-21 02:21:23 +0300

    clear

--- main.py
+++ main.py
@@ -29,7 +29,4 @@ def main():
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    try:
-        main()
-    except KeyboardInterrupt:
-        pass
+    main()

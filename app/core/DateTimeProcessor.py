155320392956ab743173bc23cd100a85bf41c28c
Artur Mustafin <funcelot@outlook.com>
2022-01-30 00:54:23 +0300

    time details

--- app/core/DateTimeProcessor.py
+++ app/core/DateTimeProcessor.py
@@ -4,7 +4,7 @@ from datetime import datetime, timedelta
 import pygame
 from core.interfaces import IGameEventProcessor
 
-date_format = "%Y-%m-%d %H:%M:%S"
+date_format = "%Y-%m-%d %H:%M:%S.%f"
 
 
 class DateTime:
@@ -17,5 +17,5 @@ class DateTimeProcessor(IGameEventProcessor):
         self.time = pygame.time.get_ticks()
 
     def process(self):
-        ticks = pygame.time.get_ticks() // 1000
-        DateTime.date = f"{datetime.strftime(datetime(1, 1, 1, 0, 0, 0) + timedelta(seconds=ticks), date_format)}"
+        ticks = pygame.time.get_ticks() / 1000
+        DateTime.date = f"{datetime.strftime(datetime(1, 1, 1, 0, 0, 0) + timedelta(seconds=ticks), date_format)}"[:-3]
